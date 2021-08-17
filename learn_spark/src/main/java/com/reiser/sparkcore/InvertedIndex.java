5c206e8bb351a6ef54e4e04b001e62596978ae16
leonzone <leonzone58@gmail.com>
2021-08-17 10:26:42 +0800

    提交 spark 作业

--- learn_spark/src/main/java/com/reiser/sparkcore/InvertedIndex.java
+++ learn_spark/src/main/java/com/reiser/sparkcore/InvertedIndex.java
@@ -5,12 +5,14 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @author: reiserx
@@ -37,45 +39,27 @@ public class InvertedIndex {
                 String line = words[1].replace("\"", "");
                 ArrayList<Tuple2<String, String>> result = new ArrayList<>();
                 for (String word : line.split(" ")) {
-                    result.add(new Tuple2<>(fileName, word));
+                    result.add(new Tuple2<>(word, fileName));
                 }
                 return result.iterator();
             }
         });
 
-        JavaPairRDD<String, List<String>> rdd = word.mapToPair(new PairFunction<Tuple2<String, String>, String, List<String>>() {
-            @Override
-            public Tuple2<String, List<String>> call(Tuple2<String, String> t) throws Exception {
-                List<String> v = new ArrayList<>();
-                v.add(t._1);
-                return new Tuple2<>(t._2, v);
+        JavaPairRDD<String, String> rdd = word.mapToPair((PairFunction<Tuple2<String, String>, String, String>) t -> new Tuple2<>(t._1, t._2));
+
+        JavaPairRDD<String, String> rdd1 = rdd.reduceByKey((Function2<String, String, String>) (v1, v2) -> v1 + "|" + v2);
+
+
+        JavaRDD<Tuple2<String, Map<String, Integer>>> rdd2 = rdd1.map(t -> {
+            Map<String, Integer> map = new HashMap<>();
+            for (String s : t._2.split("\\|")) {
+                map.put(s, map.getOrDefault(s, 0) + 1);
             }
+            return new Tuple2<>(t._1, map);
         });
-
-        JavaPairRDD<String, List<String>> rdd1 = rdd.reduceByKey(new Function2<List<String>, List<String>, List<String>>() {
-            @Override
-            public List<String> call(List<String> v1, List<String> v2) throws Exception {
-                v1.addAll(v2);
-                return v1;
-            }
-        });
-
-        JavaRDD<Tuple2<String, Map<String, Integer>>> rdd2 = rdd1.map(new Function<Tuple2<String, List<String>>, Tuple2<String, Map<String, Integer>>>() {
-            @Override
-            public Tuple2<String, Map<String, Integer>> call(Tuple2<String, List<String>> t) throws Exception {
-                Map<String, Integer> map = new HashMap<>();
-                for (String s : t._2) {
-                    map.put(s, map.getOrDefault(s, 0) + 1);
-                }
-                return new Tuple2<>(t._1, map);
-            }
-        });
-
-
         for (Tuple2<String, Map<String, Integer>> next : rdd2.collect()) {
             System.out.println("\"" + next._1 + "\": " + next._2);
         }
-
         sc.close();
     }
 }
